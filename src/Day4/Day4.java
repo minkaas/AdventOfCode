d97ba3edf80c7c727c5fc4759d1e753d55478494
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2021-12-04 10:56:00 +0100

    Restarting day 4 (y)

---
+++ src/Day4/Day4.java
@@ -0,0 +1,26 @@
+package Day4;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day4 {
+    public static void main(String[] args) {
+        BufferedReader reader;
+        File input = new File("src/Day4/input");
+        try {
+            reader = new BufferedReader(new FileReader(input));
+            String line = reader.readLine();
+            while (line != null) {
+                /**
+                 * Do some stuff here tomorrow
+                 */
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
