efda9b7ef2f9aaeed220c14d28905e4ee5148044
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2021-12-04 13:59:16 +0100

    Some preparation for day 5 and 6

---
+++ src/Day5/Day5.java
@@ -0,0 +1,27 @@
+package Day5;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Day5 {
+    public static void main(String[] args) {
+        BufferedReader reader;
+        File input = new File("src/Day5/input");
+        try {
+            reader = new BufferedReader(new FileReader(input));
+            String line = reader.readLine();
+            while (line != null) {
+                /**
+                 * Do some stuff here tomorrow
+                 */
+                line = reader.readLine();
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
