7fd3c173cd36d1e8fdb8dd2d643e2a1fb14ef0a2
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2021-12-08 06:13:11 +0100

    brak part 1 van dag 8

--- src/Day8/Day8.java
+++ src/Day8/Day8.java
@@ -4,24 +4,30 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-
+// 2 3 7 of 4
 public class Day8 {
     public static void main(String[] args) {
         Day8 d = new Day8();
         BufferedReader reader;
         File input = new File("src/Day8/input");
+        int counter = 0;
         try {
             reader = new BufferedReader(new FileReader(input));
             String line = reader.readLine();
             while (line != null) {
-                /**
-                 * Do some stuff here tomorrow
-                 */
+                String[] inputs = line.split(" . ");
+                String[] displayed = inputs[1].split(" ");
+                for (String dis : displayed) {
+                    if (dis.length() == 2 || dis.length() == 3 || dis.length() == 4 || dis.length() == 7) {
+                        counter++;
+                    }
+                }
                 line = reader.readLine();
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(counter);
     }
 }
