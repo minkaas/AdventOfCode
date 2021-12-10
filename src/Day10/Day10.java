b3c2c4b9186b6e82504f12aaac665d5d24d50dd3
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2021-12-10 06:31:16 +0100

    Finished day 10 :sunglasses: :thumbsup:

--- src/Day10/Day10.java
+++ src/Day10/Day10.java
@@ -4,8 +4,73 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day10 {
+    char[][] allCharacters = new char[102][150];
+
+    // ): 3 points.
+    // ]: 57 points.
+    // }: 1197 points.
+    // >: 25137 points
+    public long completionScore(char[] line) {
+        long result = 0;
+        ArrayList<Character> open = new ArrayList<>();
+        for (char a : line) {
+            switch (a) {
+                case '(', '[', '{', '<' -> open.add(a);
+                case ')', ']', '}', '>' -> open.remove(open.size() - 1);
+            }
+        }
+        for (int i = open.size() - 1; i >= 0; i--) {
+            char b = open.get(i);
+            result = result * 5;
+            switch (b) {
+                case '(' -> result += 1;
+                case '[' -> result += 2;
+                case '{' -> result += 3;
+                case '<' -> result += 4;
+            }
+        }
+        return result;
+    }
+
+    public boolean checkLine(char[] line) {
+        ArrayList<Character> open = new ArrayList<>();
+        char b;
+        for (char a : line) {
+            switch (a) {
+                case '(', '[', '{', '<' -> open.add(a);
+                case ')' -> {
+                    b = open.remove(open.size() - 1);
+                    if (b != '(') {
+                        return false;
+                    }
+                }
+                case ']' -> {
+                    b = open.remove(open.size() - 1);
+                    if (b != '[') {
+                        return false;
+                    }
+                }
+                case '}' -> {
+                    b = open.remove(open.size() - 1);
+                    if (b != '{') {
+                        return false;
+                    }
+                }
+                case '>' -> {
+                    b = open.remove(open.size() - 1);
+                    if (b != '<') {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         Day10 d = new Day10();
         BufferedReader reader;
@@ -13,15 +78,25 @@ public class Day10 {
         try {
             reader = new BufferedReader(new FileReader(input));
             String line = reader.readLine();
+            int i = 0;
             while (line != null) {
-                /**
-                 * Do some stuff here tomorrow
-                 */
+                d.allCharacters[i] = line.toCharArray();
                 line = reader.readLine();
+                i++;
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        long result = 0;
+        ArrayList<Long> scores = new ArrayList<>();
+        for (char[] lines : d.allCharacters) {
+            if (d.checkLine(lines)) {
+                scores.add(d.completionScore(lines));
+            }
+        }
+        Collections.sort(scores);
+        long middleScore= scores.get(scores.size() / 2);
+        System.out.println(middleScore);
     }
 }
