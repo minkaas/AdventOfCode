effd5a0e73f4848f486da792f6c3cea4f7262ddb
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2021-12-11 07:07:30 +0100

    Part 1 for day 11 is done

--- src/Day11/Day11.java
+++ src/Day11/Day11.java
@@ -4,8 +4,78 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Day11 {
+    int[][] allOctopi = new int[10][10];
+    ArrayList<int[]> explodedAt = new ArrayList<>();
+    int steps = 100;
+
+    public int oneStep() {
+        int flashes = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                allOctopi[i][j]++;
+            }
+        }
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (allOctopi[i][j] > 9) {
+                    flashes += explodeAt(new int[]{i, j}).size();
+                }
+            }
+        }
+        for (int[] a : explodedAt) {
+            allOctopi[a[0]][a[1]] = 0;
+        }
+        explodedAt.clear();
+        return flashes;
+    }
+    public boolean containsArray(int[] b) {
+        for (int[] a : explodedAt) {
+            if (Arrays.equals(a, b)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<int[]> explodeAt(int[] arr) {
+        ArrayList<int[]> explodedNow = new ArrayList<>();
+        ArrayList<int[]> result = new ArrayList<>();
+        explodedAt.add(arr);
+        explodedNow.add(arr);
+        allOctopi[arr[0]][arr[1]] = -10;
+        for (int i = arr[0] - 1; i <= arr[0] + 1; i++) {
+            for (int j = arr[1] - 1; j<= arr[1] + 1; j++) {
+                if (i >= 0 && i < 10 && j >= 0 && j < 10 && (!containsArray(new int[]{i, j}))) {
+                    allOctopi[i][j]++;
+                    if (allOctopi[i][j] > 9 && (!containsArray(new int[]{i, j}))) {
+                        explodedNow.addAll(explodeAt(new int[]{i, j}));
+                    }
+                }
+            }
+        }
+        for (int[] b : explodedNow) {
+            boolean c = true;
+            for (int[] d : result) {
+                if (Arrays.equals(b, d)) {
+                    c = false;
+                    break;
+                }
+            }
+            if (c) {
+                result.add(b);
+            }
+        }
+        for (int[] a : result) {
+            allOctopi[a[0]][a[1]] = 0;
+        }
+        explodedAt.addAll(result);
+        return result;
+    }
+
     public static void main(String[] args) {
         Day11 d = new Day11();
         BufferedReader reader;
@@ -13,15 +83,23 @@ public class Day11 {
         try {
             reader = new BufferedReader(new FileReader(input));
             String line = reader.readLine();
+            int i = 0;
             while (line != null) {
-                /**
-                 * Do some stuff here tomorrow
-                 */
+                String[] temp = line.split("");
+                for (int j = 0; j < temp.length; j++) {
+                    d.allOctopi[i][j] = Integer.parseInt(temp[j]);
+                }
+                i++;
                 line = reader.readLine();
             }
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        int sum = 0;
+        for (int i = 0; i < d.steps; i++) {
+            sum += d.oneStep();
+            System.out.println(sum);
         }
     }
 }
