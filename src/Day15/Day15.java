a29dee9a3e0f713b10a8f8111c8e88bda142252c
s2457512 <m.w.bohlmeijer@student.utwente.nl>
2021-12-13 22:17:12 +0100

    Set up for day 14 and 15

--- src/Day15/Day15.java
+++ src/Day15/Day15.java
@@ -1,15 +1,15 @@
-package Day14;
+package Day15;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class Day14 {
+public class Day15 {
     public static void main(String[] args) {
-        Day14 d = new Day14();
+        Day15 d = new Day15();
         BufferedReader reader;
-        File input = new File("src/Day14/input");
+        File input = new File("src/Day15/input");
         try {
             reader = new BufferedReader(new FileReader(input));
             String line = reader.readLine();
